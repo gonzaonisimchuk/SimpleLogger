37d3fd4d7680e08945c1d7fad52692e9cfcf5714
German Arduino <garduino@gmail.com>
2017-05-15 23:15:59 -0300

    Versión 0.0.1 is here!

--- example.js
+++ example.js
@@ -1,6 +1,16 @@
 var simplelogger = require('./lib/simplelogger');
 
-// var log = simplelogger.createLogFile();
-
+// For start the loging process
 simplelogger.createLogFile();
+
+// For log a message
+simplelogger.logMessage("I want to log this message!");
+simplelogger.logMessage("Another message here");
+simplelogger.logMessage("One more message to be logged");
+
+
+// For log a separator line
+simplelogger.logSeparator();
+
+// For end the loging process
 simplelogger.closeLogFile();
